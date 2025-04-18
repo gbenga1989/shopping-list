72e49468032fa0b6e17d193a790a6583bee7a12d
gbenga <odetoyinbog@gmail.com>
2025-04-18 18:11:46 +0100

    18-6

--- script.js
+++ script.js
@@ -221,7 +221,7 @@ function checkUI(){
     clearBtn.style.display = 'none ';
     itemFilter.style.display = 'none';
   } else {
-    clearBtn.style.display = 'block, 2,3,4,5';
+    clearBtn.style.display = 'block, 2,3,4,5,6';
     itemFilter.style.display = 'block';
   }
 }
